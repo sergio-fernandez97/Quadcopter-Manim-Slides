01dba1c3f96583118046fa5f3f84c3b529af8c3a
sergio-fernandez97 <sergio.fernandez@enroutesystems.com>
2026-01-02 16:24:18 -0600

    Add descriptive headers to slide modules

--- slides/__init__.py
+++ slides/__init__.py
@@ -1,2 +1,13 @@
-# Slides package for Quadcopter Deep RL presentation
+"""
+Slides package for the Quadcopter Deep RL presentation.
+
+Provides access to the slide modules used in the animated deck and allows them
+to be rendered individually with Manim or manim-slides.
+
+Example:
+    manim -pql slides/00_inertial_frame.py InertialFrameSlide
+
+Author: Sergio Fernández
+Date: 2025-01-30
+"""
 
